bc849ca03cef35a68dbc30361bfebe0585084935
Shakthi Thanigaivel <65614117+Shakthi011001@users.noreply.github.com>
2021-07-30 02:36:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const ejsMate = require("ejs-mate");
-const port= 3000
+const port = 3001;
 const app = express();
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
@@ -9,18 +9,18 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.send('home')
-  });
+    res.render("land");
+});
 app.get("/find", (req, res) => {
-    res.render('find')
-  });
-app.get("/land", (req, res) => {
-    res.render('land')
-  });
-
-
+    res.render("find");
+});
+app.get("/found", (req, res) => {
+    res.render("found");
+});
+app.get("/detect", (req,res) => {
+  res.render("detect")
+})
 
 app.listen(port, () => {
-    console.log("Serving on port 3000");
-  });
-  
+    console.log("Serving on port 3001");
+});
